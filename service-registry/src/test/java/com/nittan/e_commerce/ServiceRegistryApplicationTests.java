adeaa732dfb7fbf253b30d697f0d401b93079f1c
nittan <nitin01kumar14@gmail.com>
2024-06-23 23:36:27 +0530

    service-registry

---
+++ service-registry/src/test/java/com/nittan/e_commerce/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nittan.e_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1209b81b7ca42ff2ff60c6db04f03e30f842ee53
nittan <nitin01kumar14@gmail.com>
2024-06-24 11:47:34 +0530

    working

--- order-management-service/src/main/java/com/nittan/e_commerce/ECommerceApplication.java
+++ order-management-service/src/main/java/com/nittan/e_commerce/ECommerceApplication.java
@@ -14,3 +14,18 @@ public class ECommerceApplication {
         SpringApplication.run(ECommerceApplication.class, args);
     }
 }
+
+
+//package com.nittan.e_commerce;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//
+//@SpringBootApplication
+//@EnableDiscoveryClient
+//public class ECommerceApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(ECommerceApplication.class, args);
+//    }
+//}
--- order-management-service/src/main/java/com/nittan/e_commerce/client/ProductClient.java
+++ order-management-service/src/main/java/com/nittan/e_commerce/client/ProductClient.java
@@ -1,17 +1,36 @@
+//package com.nittan.e_commerce.client;
+//
+//import com.netflix.discovery.converters.Auto;
+//import com.nittan.e_commerce.entity.Product;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//
+//import java.util.List;
+//
+//@FeignClient(name = "PRODUCT-SERVICE", url = "${product.service.url}")
+//public interface ProductClient {
+//
+//    @PostMapping("/product/getProductsForOrder")
+//    ResponseEntity<List<Product>> getProductsForOrder(@RequestBody List<Long> productIds);
+//}
+
+
 package com.nittan.e_commerce.client;
 
-import com.netflix.discovery.converters.Auto;
 import com.nittan.e_commerce.entity.Product;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "product-service", url = "${product.service.url}")
+@FeignClient(name = "PRODUCT-CATALOG-SERVICE")
 public interface ProductClient {
 
     @PostMapping("/product/getProductsForOrder")
-    List<Product> getProductsForOrder(@RequestBody List<Long> productIds);
+    ResponseEntity<List<Product>> getProductsForOrder(@RequestBody List<Long> productIds);
 }

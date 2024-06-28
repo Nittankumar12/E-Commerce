6764d5ce1ac00f704ea37128c52312990e9f0378
nittan <nitin01kumar14@gmail.com>
2024-06-28 13:12:30 +0530

    auth added

--- user-authentication-service/src/main/java/com/nittan/user_authentication_service/controller/AuthController.java
+++ user-authentication-service/src/main/java/com/nittan/user_authentication_service/controller/AuthController.java
@@ -4,9 +4,6 @@ import com.nittan.user_authentication_service.dto.UserAuthRequest;
 import com.nittan.user_authentication_service.entity.UserCredential;
 import com.nittan.user_authentication_service.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController

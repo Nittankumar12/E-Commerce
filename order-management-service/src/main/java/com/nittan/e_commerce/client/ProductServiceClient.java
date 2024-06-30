88b40fe5188c0c80a7b3e90346e38a2a5e5a26fc
nittan <nitin01kumar14@gmail.com>
2024-06-30 21:26:59 +0530

    exception added

---
+++ order-management-service/src/main/java/com/nittan/e_commerce/client/ProductServiceClient.java
@@ -0,0 +1,53 @@
+package com.nittan.e_commerce.client;
+
+import com.nittan.e_commerce.dto.OrderResponseDto;
+import com.nittan.e_commerce.entity.Product;
+import com.nittan.e_commerce.exception.ProductServiceException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Component
+@Slf4j
+public class ProductServiceClient {
+    @Autowired
+    private RestTemplate template;
+
+    public ResponseEntity<List<Product>> getProductsForOrder(String orderId) {
+        List<Product> products=null;
+        try {
+            products = template.getForObject("http://PRODUCT-CATALOG-SERVICE/product/getProductsForOrder/", List.class);
+
+        } catch (HttpServerErrorException errorException) {
+            log.error("ProductServiceClient::Getting products caught the HttpServer server error {}", errorException.getResponseBodyAsString());
+            throw new ProductServiceException(errorException.toString());
+        } catch (Exception ex) {
+            log.error("ProductServiceException::Generice exception caught the generic error {}", ex.getMessage());
+            throw new ProductServiceException(ex.toString());
+
+        }
+        return new ResponseEntity<>(products,HttpStatus.OK);
+    }
+}
+
+
+
+
+/*
+
+package com.nittan.e_commerce.client;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+
+
+ */
---
+++ order-management-service/src/main/java/com/nittan/e_commerce/dto/GlobalErrorCode.java
@@ -0,0 +1,6 @@
+package com.nittan.e_commerce.dto;
+
+public class GlobalErrorCode {
+    public static final String ERROR_ORDER_NOT_FOUND = "ORDER-SERVICE-1000";
+    public static final String GENERIC_ERROR = "ORDER-SERVICE-1001";
+}
